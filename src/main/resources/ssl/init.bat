3ce1f8ec9343728407ef1ab7b7b80e7ae92c17bc
forsrc <forsrc@gmail.com>
2020-08-27 00:12:58 +0900

    ssl ca

--- src/main/resources/ssl/init.bat
+++ src/main/resources/ssl/init.bat
@@ -5,8 +5,15 @@ rem keytool -alias %NAME% -exportcert -keystore %NAME%.jks -file %NAME%.cer -sto
 rem keytool -import -alias %NAME% -keystore %JAVA_HOME%\jre\lib\security\cacerts -file %NAME%.cer -trustcacerts -storepass %PASS%
 
 
+keytool -delete -alias %NAME%.server -keystore %NAME%.server.keystore -storepass %PASS%
+keytool -delete -alias %NAME%.client -keystore %NAME%.server.keystore -storepass %PASS%
+keytool -list -v -keystore %NAME%.server.keystore -storepass %PASS%
+rem pause
+
 rem 1.
 keytool -genkey -alias %NAME%.server -dname "CN=%NAME%,OU=forsrc,O=forsrc,C=CN" -ext "san=DNS:%NAME%" -keypass %PASS% -keyalg RSA -keysize 1024 -validity 365 -keystore %NAME%.server.keystore -storepass %PASS% 
+keytool -importkeystore -srckeystore %NAME%.server.keystore -destkeystore %NAME%.server.p12 -srcstoretype JKS -deststoretype PKCS12 -srcstorepass %PASS% -deststorepass %PASS% -srckeypass %PASS% -destkeypass %PASS% -srcalias %NAME%.server -destalias %NAME%.server -noprompt
+
 
 rem 2.
 keytool -genkey -alias %NAME%.client -dname "CN=%NAME%,OU=forsrc,O=forsrc,C=CN" -ext "san=DNS:%NAME%" -keypass %PASS% -keyalg RSA -keysize 1024 -validity 365 -storetype PKCS12 -keystore %NAME%.client.p12 -storepass %PASS%
@@ -15,7 +22,7 @@ keytool -genkey -alias %NAME%.client -dname "CN=%NAME%,OU=forsrc,O=forsrc,C=CN" 
 rem 3.
 keytool -export -alias %NAME%.client -keystore %NAME%.client.p12 -storetype PKCS12 -keypass %PASS% -file %NAME%.client.cer -storepass %PASS%
 
-keytool -delete -alias %NAME%.client -keystore %NAME%.server.keystore -storepass %PASS%
+
 keytool -import -alias %NAME%.client -file %NAME%.client.cer -keystore %NAME%.server.keystore -storepass %PASS% -trustcacerts -noprompt
 keytool -list -v -keystore %NAME%.server.keystore -storepass %PASS%
 
