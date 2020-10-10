e77c6c49fa7579a6b450915c7da2fcf081123293
forsrc <forsrc@gmail.com>
2020-10-10 20:58:53 +0900

    Create init.sh

---
+++ kurento-media-server/init.sh
@@ -0,0 +1,6 @@
+docker run --name kurento-media-server \
+    -p 8888:8888/tcp \
+    -p 5000-5050:5000-5050/udp \
+    -e KMS_MIN_PORT=5000 \
+    -e KMS_MAX_PORT=5050 \
+    kurento/kurento-media-server:latest
